b0ed0b87bc134d3ba2dd4cc0b2107466acdeecdc
asalbores <asalbores@up.edu.ph>
2016-09-30 19:32:11 +0800

    Create MyList.java

---
+++ laboratory5/MyList.java
@@ -0,0 +1,15 @@
+/**
+ * Allyssa Albores
+ * Cristina Dayuday
+ */
+public interface MyList {
+  
+  void add(Object item);
+  void add(int i, Object item);
+  Object get(int i);
+  void remove(int i);
+  void remove(Object item);
+  void set(int i, Object item);
+  
+}
+  
